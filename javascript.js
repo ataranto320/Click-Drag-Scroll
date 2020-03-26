fe2085cfd46384b1eb6baf58dbe688f82af15002
Austin T <austin.taranto@gmail.com>
2020-03-26 15:50:41 -0400

    slider mouse movements

--- javascript.js
+++ javascript.js
@@ -3,3 +3,19 @@ let isDown = false;
 let startX;
 let ScrollLeft;
 
+slider.addEventListener("mousedown", () => {
+    isDown = true;
+});
+
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+});
+
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+});
+
+slider.addEventListener("mousemove", () => {
+    console.log(isDown);
+    console.log("do work");
+});
