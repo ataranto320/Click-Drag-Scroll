90465757a5c68cef0ca56f434bbeeec19650fe03
Austin T <austin.taranto@gmail.com>
2020-03-26 16:00:00 -0400

    mouse down update

--- javascript.js
+++ javascript.js
@@ -3,9 +3,12 @@ let isDown = false;
 let startX;
 let ScrollLeft;
 
-slider.addEventListener("mousedown", () => {
+slider.addEventListener("mousedown", (e) => {
     isDown = true;
     slider.classList.add("active");
+    // console.log(e.pageX);
+    startX = e.pageX - slider.offsetLeft;
+    console.log(startX);
 });
 
 slider.addEventListener("mouseleave", () => {
@@ -22,6 +25,7 @@ slider.addEventListener("mousemove", () => {
     // stop the function from running
     if (!isDown) return; 
     console.count(isDown);
+    console.log(startX);
     // console.log(isDown);
     // console.log("do work");
 });
